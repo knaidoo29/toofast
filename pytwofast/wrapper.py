b6fa6cdaa6fb89fd1a9571c13faf50c34208a280
Krishna Naidoo <krishna@krish-macbook.local>
2019-09-10 21:16:16 +0100

    update

--- pytwofast/wrapper.py
+++ pytwofast/wrapper.py
@@ -96,9 +96,9 @@ class TwoPoint:
             subprocess.call(twopoint + " " +self.path+ "temp_paramfile.ini", shell=True)
         else:
             if location == 'splinter':
-            subprocess.call(mpirun + " --machinefile $PBS_NODEFILE -x PATH -x LD_LIBRARY_PATH " + twopoint_mpi + " " +self.path+ "temp_paramfile.ini", shell=True)
+                subprocess.call(mpirun + " --machinefile $PBS_NODEFILE -x PATH -x LD_LIBRARY_PATH " + twopoint_mpi + " " +self.path+ "temp_paramfile.ini", shell=True)
             else:
-            subprocess.call(mpirun + " -n " + str(processors) + " " + twopoint_mpi + " " +self.path+ "temp_paramfile.ini", shell=True)
+                subprocess.call(mpirun + " -n " + str(processors) + " " + twopoint_mpi + " " +self.path+ "temp_paramfile.ini", shell=True)
 
     def clean(self, remove_temp_files=True):
         if remove_temp_files == True:
