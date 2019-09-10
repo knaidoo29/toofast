f5fe5c2f8194f89a5e93f0c0d968ecd6276ca197
Krishna Naidoo <krishna@krish-macbook.local>
2019-09-10 19:59:53 +0100

    add splinter paths for python run

--- pytwofast/source.py
+++ pytwofast/source.py
@@ -4,4 +4,8 @@ def get_src(location):
         mpirun = '/Users/krishna/Programs/build/openmpi/bin/mpirun'
         twopoint = '/Users/krishna/Research/github/twofast/TWOPOINT'
         twopoint_mpi = '/Users/krishna/Research/github/twofast/TWOPOINT_MPI'
+    elif location == 'splinter':
+        mpirun = '/opt/openmpi/bin/mpirun'
+        twopoint = '/share/data1/knaidoo/splinter_libraries/src/twofast/TWOPOINT'
+        twopoint_mpi = '/share/data1/knaidoo/splinter_libraries/src/twofast/TWOPOINT_MPI'
     return mpirun, twopoint, twopoint_mpi
